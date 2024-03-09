afb1e13e7a5c8810b33ccdcc6531e854fb10d294
Abdalla Abdelsabour <abdotlos60@gmail.com>
2024-03-09 19:15:30 +0200

    add

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,5 @@
-"use client";
 import HeroSection from "@/app/components/HomePage/HeroSection";
-import PopularCourses from "@/app/components/HomePage/PopularCourses";
-import HomeAbout from "@/app/components/HomePage/AboutUs";
-import SectionContainer from "@/app/components/HomePage/SectionContainer";
-import React, { createContext, useState } from "react";
-
-export const AboutSectionContext = createContext(null);
 
 export default function Home() {
-  const [animation, setAnimation] = useState();
-
-  return (
-    <AboutSectionContext.Provider value={{ animation, setAnimation }}>
-      <HeroSection />
-      <SectionContainer>
-        <HomeAbout />
-        <PopularCourses />
-      </SectionContainer>
-    </AboutSectionContext.Provider>
-  );
+  return <HeroSection />;
 }
--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,77 +1,9 @@
-"use client";
-import AppSearchAPIConnector from "@elastic/search-ui-app-search-connector";
-import React from "react";
-import {
-  ErrorBoundary,
-  Paging,
-  PagingInfo,
-  Results,
-  ResultsPerPage,
-  SearchBox,
-  SearchProvider,
-  WithSearch,
-} from "@elastic/react-search-ui";
-import { Layout } from "@elastic/react-search-ui-views";
-import "@elastic/react-search-ui-views/lib/styles/styles.css";
+import Search from "./Search";
 
-const connector = new AppSearchAPIConnector({
-  searchKey: "search-epvktg6orw2tj42sun1vmgww",
-  engineName: "news",
-  endpointBase:
-    "https://513076c5669b4aa2af5b0cd85cc3a74e.ent-search.us-central1.gcp.cloud.es.io",
-});
-const config = {
-  alwaysSearchOnInitialLoad: true,
-  apiConnector: connector,
-  hasA11yNotifications: true,
-  searchQuery: {
-    result_fields: {
-      title: { raw: {} },
-    },
-    search_fields: {
-      title: {},
-    },
-    disjunctiveFacets: [""],
-    facets: {},
-  },
-};
-
-export default function App() {
+export default function Page() {
   return (
-    <SearchProvider config={config}>
-      <WithSearch
-        mapContextToProps={({ wasSearched }) => ({
-          wasSearched,
-        })}
-      >
-        {({ wasSearched }) => {
-          return (
-            <div className="App">
-              <ErrorBoundary>
-                <Layout
-                  header={<SearchBox debounceLength={0} />}
-                  sideContent={<div></div>}
-                  bodyContent={
-                    <Results
-                      titleField="title"
-                      urlField="nps_link"
-                      thumbnailField="image_url"
-                      shouldTrackClickThrough
-                    />
-                  }
-                  bodyHeader={
-                    <React.Fragment>
-                      {wasSearched && <PagingInfo />}
-                      {wasSearched && <ResultsPerPage />}
-                    </React.Fragment>
-                  }
-                  bodyFooter={<Paging />}
-                />
-              </ErrorBoundary>
-            </div>
-          );
-        }}
-      </WithSearch>
-    </SearchProvider>
+    <div>
+      <Search />
+    </div>
   );
 }
